b7225c16cd816e276d521002ca1a148368b63895
Divya narayanagari <djspy.java@gmail.com>
2022-01-20 15:53:12 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "linux_web_server" {
   ami           = "${lookup(var.webserver_amis,var.aws_region )}"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0fb738bbc6a435ae0"
   tags = {
     Name = "Linux_server"
   }
